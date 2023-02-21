d89a9a6b1f2a9d155db9e0a6748863272c2de8bf
Porter Lyman <dev@porterlyman.com>
2023-02-20 23:13:12 -0700

    Getting local delete to work

--- src/DeckPage/DeckOverview.js
+++ src/DeckPage/DeckOverview.js
@@ -2,7 +2,7 @@
 //buttons: edit, study, add, delete
 
 //will display deck with description, buttons: view, study, delete (link)
-import React from "react";
+import React, {useEffect} from "react";
 //import DeleteDeck from './DeckList'
 import { deleteDeck } from "../utils/api";
 import {useRouteMatch, useParams} from 'react-router-dom'
@@ -10,29 +10,24 @@ import CardView from './CardView'
 
 //make buttons
 
-function DeckOverview({deck, deleteCardLocal}){
+function DeckOverview({deck, deleteCardLocalParent}){
     const {deckId} = useParams(); // TODO: This ID will need to be pulled from parameters.
     const {url, path} = useRouteMatch();
-    console.log(deck)
   //handle delete
   const handleDelete = async () =>
   {
     await deleteDeck(deck.id)   
   }
 
-  const handleDeleteLocal =  (index) =>
-  {
-    console.log('yoo', index)
-  
-
+  function handleDeleteLocal(cardId) {
+    deleteCardLocalParent(cardId)
   }
 
-
-  const cardList = (deck.cards).map((card, index)=> 
+  const cardList = (deck.cards)?.map((card, index)=> 
   <CardView card={card}
             key={index}
             url={url}
-            deleteCardLocal = {() => deleteCardLocal(index)}
+            deleteCardLocal = {() => handleDeleteLocal(index)}
             />)
 
     return (
@@ -43,7 +38,7 @@ function DeckOverview({deck, deleteCardLocal}){
                 <p className = 'card-desc'>{deck.description}</p>
                 <a className="view-btn" href={`/decks/${deck.id}/edit`}>Edit</a> 
                 <a className="study-btn" href={`/decks/${deck.id}/study`}>Study</a>
-                {/* <button className="delete-btn" onClick={handleDelete}>Delete</button> */}
+                <button className="delete-btn" onClick={handleDelete}>Delete</button>
             </div>
         </div>
         <h2>Cards</h2>
